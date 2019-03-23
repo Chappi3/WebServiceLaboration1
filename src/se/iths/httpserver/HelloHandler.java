7341e0d621ec29d87f829d5bb13c02501c57186b
Tonny <tonny035@hotmail.com>
2019-03-23 11:46:51 +0100

    Add class HelloHandler with handle method

---
+++ src/se/iths/httpserver/HelloHandler.java
@@ -0,0 +1,11 @@
+package se.iths.httpserver;
+
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class HelloHandler {
+    public void handle(Socket socket, String fileRequested, PrintWriter out, BufferedOutputStream dataOut) {
+
+    }
+}
